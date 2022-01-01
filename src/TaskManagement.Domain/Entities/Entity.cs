640479a8d5dc1388397c72f4328cd0e1dede2789
Lucas <lucasgunner3@gmail.com>
2022-01-01 15:44:33 -0300

    Criando entidade base

---
+++ src/TaskManagement.Domain/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TaskManagement.Domain.Entities
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public virtual Guid Id  { get; }
+
+        public bool Equals(Entity other)
+        {
+            return Id.Equals(other?.Id);
+        }
+    }
+}
